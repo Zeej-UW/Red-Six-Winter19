2dea6665361ee5d3537dd1087fdbff5f47c02c14
Charles Jackson <cjjaxx@uw.edu>
2019-01-23 19:54:02 -0800

    Added enemies.js and damage.js to function for more structure in project rather than having it centralized in main.js

---
+++ damage.js
@@ -0,0 +1,10 @@
+// Generic damage object that can be used for all attacks.
+function damage(game, stunDuration, invDuration, damageType, debuff) {
+    this.game = game
+    this.stunDuration = stunDuration
+    this.invDuration = invDuration
+    this.damageType = damageType
+    this.debuff = debuff
+    
+}
+
---
+++ enemies.js
@@ -0,0 +1,15 @@
+/*
+-PH- Fill out with different types of enemies. Wasn't sure if we wanted to have a universal js file full of 
+our enemies of individual files for each enemy type. If the former, I think it would make attaching functions
+to the prototypes a bit easier since it's in one file. 
+*/
+function enemy_1(game) {
+	this.damage 
+}
+
+/*
+-PH-
+*/
+function attack(game, damage, animation) {
+
+}
